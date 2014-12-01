2c756ece9066abf9a79d797c12187b189fbfc299
Chase Bradbury <chasebradbury@gmail.com>
2014-11-30 21:45:05 -0800

    Added header comment block for Player.js

--- Player.js
+++ Player.js
@@ -1,36 +1,63 @@
-/*Player class:
+/*** PLAYER CLASS ***
+
+ Player class: Character controller for player character. Handles collision, character update, and display of character sprite.
  * All parameters passed in are based on cellSize, the height should be between 1 and 2 in most cases.
- * startX and startY define what grid cell the player's top left coordinate initially is.
- * x and y define what grid cell the player's top left coordinate is.
- * height is the size of the player in units of cellSize.
+ 
+ Members
+ * startX and startY define where in pixels the player's top left coordinate initially is. This is per level and checkpoint and determines where the player will restart. Note: converted into pixels from startX and startY parameters, which are based on grid placement.
+ * width and height define the size in pixels of the player in units of cellSize. Note: height converted into pixels from height parameter, which is based on grid cell size.
+ * x and y define where in pixels the player's top left coordinate is on each update. Used to display sprite and in collision as the left and top coordinates respectively.
+ * bottom and right define where in pixels the bottom and right collision coordinates are with respect to the x and y coordinates.
  * speedX is how fast the player is moving horizontally. Affected by the right and left arrow keys.
  * speedY is how fast the player is moving vertically. Affected by the up arrow, collision, and gravity.
- * jumping is a boolean that says whether player is currently jumping. Player can only jump when jumping is false.
- * onGround is a boolean that determines if player is affected by gravity.
+ * platformSpeed is taken from the horizontal speed of the platform on which the character is standing.
+ * onGround is a boolean flag that determines if player is affected by gravity. Triggered by a collision from below.
+ * hitLeft, hitRight, and hitUp are boolean flags triggered by a collision from the left, right, or above, respectively.
+ * dead is a boolean flag that is triggered when the character collides with a deadly object, or has two opposite direction hits flagged.
+ * turnRight flags whether the character is looking right. Helps draw character facing the correct direction, independent of movement speed.
+ * movingLeft and movingRight are boolean flags that are triggered when the player pressed the left or right arrow keys, respectively. Checking these helps ensure that if both are held down the character does not move.
+ * frame is the index of the character's frame animation. Updated every draw call.
+ 
+ Functions
+ * collides(object): Takes another object and checks whether its coordinates overlap with the player. Sets a hit flag for each side that is hit, if not already flagged.
+ * update: To be called after all collision checks have been completed. Updates the character position and state based on hit flags.
+ * draw: To be called after update. Draws the character sprite to the screen.
  */
+
 function Player(startX, startY, height) {
+    
+    // Starting location
     this.startX = startX * cellSize;
     this.startY = startY * cellSize;
+    
+    // Size
     this.width = 25;
     this.height = height * cellSize;
+    
+    // Current position and collision boundaries
     this.x = this.startX;
     this.y = this.startY;
+    this.bottom = this.y + this.height;
+    this.right = this.x + this.width;
+    
+    // Current speed
     this.speedX = 0;
     this.speedY = 0;
-    this.onGround = false;
-    this.turnRight = true;
-    this.movingLeft = false;
-    this.movingRight = false;
+    this.platformSpeed = 0;
     
+    // Hit flags
+    this.onGround = false;
     this.hitRight = false;
     this.hitLeft = false;
     this.hitTop = false;
     
-    this.bottom = this.y + this.height;
-    this.right = this.x + this.width;
-    this.platformSpeed = 0;
+    // Other flags
     this.dead = false;
+    this.turnRight = true;
+    this.movingLeft = true;
+    this.movingRight = true;
     
+    // Current frame
     this.frame = 0;
     
     
@@ -47,21 +74,21 @@ function Player(startX, startY, height) {
            			this.bottom = obj2.y + obj2.fSpeedY();
                 	this.y = obj2.y + obj2.fSpeedY() - this.height;
            		} else{*/
-           			this.speedY = 0;
+                this.speedY = 0;
            			//if ((obj2.bottom > obj2.negLim && obj2.vertical && obj2.y < obj2.posLim)){
-           			if(!obj2.deadly){
-           			this.bottom = obj2.y;
-                	this.y = obj2.y - this.height;
-                	}
+                if(!obj2.deadly){
+                    this.bottom = obj2.y;
+                    this.y = obj2.y - this.height;
+                }
                 //}
            		//}
            		if (!this.onGround){
            			if (series == 0 || series == 2){
-           			snd_player_land_snow.currentTime = 0;
-           			snd_player_land_snow.play();
+                        snd_player_land_snow.currentTime = 0;
+                        snd_player_land_snow.play();
            			}else{
-           			snd_player_land.currentTime = 0;
-           			snd_player_land.play();
+                        snd_player_land.currentTime = 0;
+                        snd_player_land.play();
            			}
            		}
                 
@@ -181,22 +208,22 @@ function Player(startX, startY, height) {
     	if (this.dead){
     		snd_player_death.play();
     		if(this.turnRight){
-    		++this.frame;
-    		this.frame %= player_dieright.length;
-        	ctx.drawImage(player_dieright[this.frame], this.x - 5, this.y);
+                ++this.frame;
+                this.frame %= player_dieright.length;
+                ctx.drawImage(player_dieright[this.frame], this.x - 5, this.y);
         	}else{
-        	++this.frame;
-    		this.frame %= player_dieleft.length;
-        	ctx.drawImage(player_dieleft[this.frame], this.x - 5, this.y);
+                ++this.frame;
+                this.frame %= player_dieleft.length;
+                ctx.drawImage(player_dieleft[this.frame], this.x - 5, this.y);
         	}
     	}else{
     		snd_player_death.pause();
     		
     	if (!this.onGround){
-    			snd_player_walk_snow.pause();
-    			snd_player_walk_snow.currentTime = 0;
-    			snd_player_walk.pause();
-    			snd_player_walk.currentTime = 0;
+            snd_player_walk_snow.pause();
+            snd_player_walk_snow.currentTime = 0;
+            snd_player_walk.pause();
+            snd_player_walk.currentTime = 0;
     		if (this.turnRight){
     			++this.frame;
     			this.frame %= player_fallright.length;
